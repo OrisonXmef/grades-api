9a689e3f6e3905b74b09677cc84d5276983c97f9
OrisonXmef <orisonmustapha@gmail.com>
2022-09-01 14:17:05 +0000

    first commit

---
+++ results.js
@@ -0,0 +1,9 @@
+const results =[{name: "Orison Mustapha", class:"final year", grade: "8"},
+                {name: "Sadika Bint Omar", class:"final year", grade: "8"},
+                {name: "Dong Leticia", class:"final year", grade: "9"},
+                {name: "Adams Adinatu", class:"final year", grade: "11"},
+                {name: "Ali Mohammed Subur", class:"final year", grade: "15"},
+                {name: "Samsideen Naeem", class:"final year", grade: "15"},
+]
+
+module.exports=results
